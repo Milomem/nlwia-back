ec99859158c82ecd6d0445a13133441a59c237bc
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2023-09-14 16:09:56 -0300

    Completed

--- src/routes/generate-ai-complition.ts
+++ src/routes/generate-ai-complition.ts
@@ -1,19 +1,19 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod"
-import { createReadStream } from "node:fs";
 import { openai } from "../lib/openai"
+import { streamToResponse, OpenAIStream } from 'ai'
 
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
     app.post('/ai/complete', async ( req, reply) => {
         const bodySchema = z.object({
-            template: z.string(),
+            prompt: z.string(),
             videoId: z.string().uuid(),
             temperatura: z.number().min(0).max(1).default(0.5)
         })
 
-        const { videoId, temperatura, template } = bodySchema.parse(req.body)
+        const { videoId, temperatura, prompt } = bodySchema.parse(req.body)
 
         const video = await prisma.video.findFirstOrThrow({
             where: {
@@ -25,16 +25,24 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             return reply.status(400).send({ error: "Video transcription was not generate"})
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription)
+        const promptMessage = prompt.replace('{transcription}', video.transcription)
 
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             temperature: temperatura,
             messages: [
                 { role: 'user', content: promptMessage}
-            ]
+            ],
+            stream: true
         })
 
-        return response
+        const stream = OpenAIStream(response)
+
+        streamToResponse(stream, reply.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            }
+        })
     })
 }
